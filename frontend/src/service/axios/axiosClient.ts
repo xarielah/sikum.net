92b921c37ca6c97e56fe15439bdfceb5fc1f00f6
arielahr <arielah1997@gmail.com>
2023-02-13 10:41:25 +0200

    Fix axios service's base URL

--- frontend/src/service/axios/axiosClient.ts
+++ frontend/src/service/axios/axiosClient.ts
@@ -5,7 +5,7 @@ const BACKEND_URL = import.meta.env.VITE_BACKEND;
  * Main axios instance for calling standard data.
  */
 export const axiosClient = axios.create({
-  baseURL: BACKEND_URL ?? "http://imadummyurl.com",
+  baseURL: BACKEND_URL ?? "http://localhost:3000/api/",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
